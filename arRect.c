809975fc80886b25cd5d9327f79424b2aad2eabb
abhishekgedelacode <gabhishekji143@gmail.com>
2020-10-29 19:57:09 +0530

    area of a rectangle

---
+++ arRect.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+	int len, wid;
+
+	printf("enter the length  of the rectangle: ");
+	scanf("%d", &len);
+	printf("enter the width of the rectangle: ");
+	scanf("%d", &wid);
+
+	printf("\n area of the given rectangle is : %d\n", len*wid);
+
+	return 0;
+}

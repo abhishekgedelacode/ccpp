6dc084e9c1fa77312188d79dc21bc19cff77e43e
abhishekgedelacode <gabhishekji143@gmail.com>
2022-02-07 22:40:36 +0530

    pattern problem

--- rough.c
+++ rough.c
@@ -1,4 +1,34 @@
 #include <stdio.h>
 int main()
 {
+    int i, j, n = 1, s, r;
+    printf("enter rows: ");
+    scanf("%d", &r);
+    for (i = 0; i < r; i++)
+    {
+        for (s = 0; s < r - i; s++)
+        {
+            printf(" ");
+        }
+        for (j = 0; j < n; j++)
+        {
+            printf("*");
+        }
+        n += 2;
+        printf("\n");
+    }
+    n -= 4;
+    for (i = 0; i < r - 1; i++)
+    {
+        for (s = 0; s <= i + 1; s++)
+        {
+            printf(" ");
+        }
+        for (j = 0; j < n; j++)
+        {
+            printf("*");
+        }
+        n -= 2;
+        printf("\n");
+    }
 }
